e65bc0802892c597b29a90c8ac15c828803c83c7
azawy-iq <anwar.nille@gmail.com>
2026-07-30 23:42:44 +0300

    Update CodeForgeApp.swift

--- CodeForge/CodeForgeApp.swift
+++ CodeForge/CodeForgeApp.swift
@@ -1,1 +1,15 @@
+//
+//  CodeForgeApp.swift
+//  CodeForge — محرر أكواد بسيط لكتابة مشاريع Swift من آيفونك
+//
 
+import SwiftUI
+
+@main
+struct CodeForgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ProjectListView()
+        }
+    }
+}
